a569ea3e3fe7920d5f74311c64683133b13a6123
Batyray <yusufcansiz1998@gmail.com>
2025-02-01 22:58:42 +0300

    fix backend url delete the old port address

--- src/stores/todo.js
+++ src/stores/todo.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-const API_URL = 'http://backend:3000/api/todos'
+const API_URL = 'http://backend/api/todos'
 
 export const useTodoStore = defineStore('todo', {
   state: () => ({
